cb09cffa9fd152de9e8e5f45cbdba25f585c41be
trommers <sha1-a079b4027524b1c85b12cfed863561188e654ea0@cern.ch>
2009-03-03 13:40:05 +0000

    changed default value

--- python/muonTOF_cfi.py
+++ python/muonTOF_cfi.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 MuonTOFFilter_trackQuality = cms.EDFilter("MuonTOFFilter_trackQuality",
     max_goodmuons = cms.double(2),
-    min_time      = cms.double(-4),
-    max_time      = cms.double(50),
+    min_time      = cms.double(-20),
+    max_time      = cms.double(20),
     max_timeError = cms.double(10)                         
  )
 
