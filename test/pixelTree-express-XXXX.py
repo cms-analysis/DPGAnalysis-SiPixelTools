c85f9566bd40b16e22fec98126a1d0997b952290
Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch>
2009-12-08 04:49:36 +0000

    the template py file for EXPRESS stream analysis

---
+++ test/pixelTree-express-XXXX.py
@@ -0,0 +1,84 @@
+import os
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+# -- Database configuration
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+process.load("CondCore.DBCommon.CondDBSetup_cfi")
+
+# -- Conditions
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("RecoVertex.BeamSpotProducer.BeamSpot_cfi")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "GR09_P_V6::All"
+
+# -- Input files
+process.source = cms.Source(
+    "PoolSource",
+    # replace with your files
+    #lastRun = cms.untracked.uint32(64789),
+    #timetype = cms.string('runnumber'),
+    #firstRun = cms.untracked.uint32(64108),
+    #interval = cms.uint32(1),
+    fileNames = cms.untracked.vstring(
+    REPLACEFILES
+    )
+    )
+
+# -- number of events
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+    )
+
+# -- Trajectory producer
+process.load("RecoTracker.TrackProducer.TrackRefitters_cff")
+process.TrackRefitter.src = 'generalTracks::EXPRESS'
+
+# ----------------------------------------------------------------------
+process.PixelFilter = cms.EDFilter(
+    "SkimEvents",
+    verbose                        = cms.untracked.int32(2),
+    filterOnPrimaryVertex          = cms.untracked.int32(1),
+    primaryVertexCollectionLabel   = cms.untracked.InputTag('offlinePrimaryVertices::EXPRESS'),
+    filterOnTracks                 = cms.untracked.int32(1),
+    trackCollectionLabel           = cms.untracked.InputTag('generalTracks::EXPRESS'),
+    filterOnPixelCluster           = cms.untracked.int32(1),
+    PixelClusterCollectionLabel    = cms.untracked.InputTag('siPixelClusters::EXPRESS'),
+    filterOnL1TechnicalTriggerBits = cms.untracked.int32(0),
+    L1TechnicalTriggerBits         = cms.untracked.vint32(40, 41)
+    )
+
+# -- the tree filler
+try:
+    rootFileName = os.environ["JOB"] + ".root"
+except KeyError:
+    rootFileName = "rfio:/castor/cern.ch/cms/store/group/tracker/pixel/PixelTree/test/pixel-test-ExpressPhysics-XXXX.root"
+
+process.PixelTree = cms.EDAnalyzer(
+    "PixelTree",
+    verbose                      = cms.untracked.int32(0),
+    rootFileName                 = cms.untracked.string(rootFileName),
+    dumpAllEvents                = cms.untracked.int32(0),
+    PrimaryVertexCollectionLabel = cms.untracked.InputTag('offlinePrimaryVertices::EXPRESS'),
+    muonCollectionLabel          = cms.untracked.InputTag('muons'),
+    trajectoryInputLabel         = cms.untracked.InputTag('TrackRefitter'),
+    trackCollectionLabel         = cms.untracked.InputTag('generalTracks::EXPRESS'),
+    pixelClusterLabel            = cms.untracked.InputTag('siPixelClusters::EXPRESS'),
+    L1GTReadoutRecordLabel       = cms.untracked.InputTag('gtDigis'), 
+    hltL1GtObjectMap             = cms.untracked.InputTag('hltL1GtObjectMap'), 
+    HLTResultsLabel              = cms.untracked.InputTag('TriggerResults::HLT')
+    )
+
+
+
+# -- Path
+process.p = cms.Path(
+    process.PixelFilter* 
+    process.TrackRefitter*
+    process.PixelTree
+    )
+
