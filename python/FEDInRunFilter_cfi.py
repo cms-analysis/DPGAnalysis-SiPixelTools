e772bb361d24406dd24f635c741148e543e3be44
cmsbuild <>
2011-06-16 12:55:53 +0000

    This commit was manufactured by cvs2git to create tag 'DPGAnalysis- SiPixelTools-ursl-110616bis'.
    Sprout from master 2011-06-16 12:55:52 UTC Urs Langenegger <sha1-e45d41b0fcfbb398e86cdb0ac3aecd7b7c65cc0c@cern.ch> 'new GT'
    Delete:
        BuildFile
        BuildFile.xml
        doc/html/index.html
        doc/html/overview.html
        macros/anaPixelTree.cc
        macros/anaPixelTree.hh
        macros/anaPtDeadRocs.cc
        macros/anaPtDeadRocs.hh
        macros/testTrigger.C
        plugins/BuildFile
        plugins/BuildFile.xml
        plugins/DetectorInformation.cc
        plugins/DetectorInformation.h
        plugins/FEDInRunFilter.cc
        plugins/MuonTOFFilter_trackQuality.cc
        plugins/PixelEfficiency.cc
        plugins/PixelNtuplizer_RealData.cc
        plugins/PixelNtuplizer_RealData.h
        plugins/PixelTree.cc
        plugins/PixelTree.h
        plugins/SkimEvents.cc
        python/FEDInRunFilter_cfi.py
        python/PixelNtuplizer_RealData_cfi.py
        python/muonTOF_cfi.py

--- python/FEDInRunFilter_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-fedInRunFilter = cms.EDFilter("FEDInRunFilter",
-                              maxBadFEDsBPIX = cms.uint32(0),
-                              maxBadFEDsFPIX = cms.uint32(0),
-                              maxBadFEDs = cms.uint32(0)
- )
-
