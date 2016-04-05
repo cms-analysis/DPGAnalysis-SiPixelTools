c37d15dc4a9bcec2beb97cf40daf33808243121f
danek kotlinski <danek.kotlinski@psi.ch>
2016-04-05 17:17:55 +0200

    minor changes

---
+++ HitAnalyzer/scripts/SimsToClus.py
@@ -0,0 +1,372 @@
+##############################################################################
+# start from simhits, do trackerlocal, compare sim and rec-hits
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Test")
+
+# process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.Geometry.GeometryDB_cff")
+
+#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+#process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.Services_cff")
+process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+
+# process.load("SimTracker.Configuration.SimTracker_cff")
+process.load("SimG4Core.Configuration.SimG4Core_cff")
+
+# for strips 
+process.load("CalibTracker.SiStripESProducers.SiStripGainSimESProducer_cfi")
+
+# clusterizers & rhs 
+process.load("RecoLocalTracker.Configuration.RecoLocalTracker_cff")
+# needed for pixel RecHits (templates?)
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
+#from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+from Configuration.AlCa.GlobalTag import GlobalTag
+# to use no All 
+
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_data', '')
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run1_data', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_mc', '')
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:run2_design', '')
+#process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2017', '')
+#process.GlobalTag = GlobalTag(process.GlobalTag, '76X_upgrade2017_design_v8', '')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# process.load("SimGeneral.MixingModule.mixNoPU_cfi")
+
+from SimGeneral.MixingModule.aliases_cfi import * 
+from SimGeneral.MixingModule.mixObjects_cfi import *
+# from SimGeneral.MixingModule.digitizers_cfi import *
+from SimGeneral.MixingModule.pixelDigitizer_cfi import *
+from SimGeneral.MixingModule.stripDigitizer_cfi import *
+from SimGeneral.MixingModule.trackingTruthProducer_cfi import *
+
+process.simSiPixelDigis = cms.EDProducer("MixingModule",
+#    digitizers = cms.PSet(theDigitizers),
+#    digitizers = cms.PSet(
+#      mergedtruth = cms.PSet(
+#            trackingParticles
+#      )
+#    ),
+
+  digitizers = cms.PSet(
+   pixel = cms.PSet(
+    pixelDigitizer
+   ),
+#  strip = cms.PSet(
+#    stripDigitizer
+#  ),
+  ),
+
+#theDigitizersValid = cms.PSet(
+#  pixel = cms.PSet(
+#    pixelDigitizer
+#  ),
+#  strip = cms.PSet(
+#    stripDigitizer
+#  ),
+#  ecal = cms.PSet(
+#    ecalDigitizer
+#  ),
+#  hcal = cms.PSet(
+#    hcalDigitizer
+#  ),
+#  castor = cms.PSet(
+#    castorDigitizer
+#  ),
+#  mergedtruth = cms.PSet(
+#    trackingParticles
+#  )
+#),
+
+
+    LabelPlayback = cms.string(' '),
+    maxBunch = cms.int32(3),
+    minBunch = cms.int32(-5), ## in terms of 25 ns
+
+    bunchspace = cms.int32(25),
+    mixProdStep1 = cms.bool(False),
+    mixProdStep2 = cms.bool(False),
+
+    playback = cms.untracked.bool(False),
+    useCurrentProcessOnly = cms.bool(False),
+    mixObjects = cms.PSet(
+        mixTracks = cms.PSet(
+            mixSimTracks
+        ),
+        mixVertices = cms.PSet(
+            mixSimVertices
+        ),
+        mixSH = cms.PSet(
+#            mixPixSimHits
+# mixPixSimHits = cms.PSet(
+    input = cms.VInputTag(cms.InputTag("g4SimHits","TrackerHitsPixelBarrelHighTof"), 
+                          cms.InputTag("g4SimHits","TrackerHitsPixelBarrelLowTof"),
+                          cms.InputTag("g4SimHits","TrackerHitsPixelEndcapHighTof"), 
+                          cms.InputTag("g4SimHits","TrackerHitsPixelEndcapLowTof"), 
+#                          cms.InputTag("g4SimHits","TrackerHitsTECHighTof"), 
+#                          cms.InputTag("g4SimHits","TrackerHitsTECLowTof"), 
+#                          cms.InputTag("g4SimHits","TrackerHitsTIBHighTof"),
+#                          cms.InputTag("g4SimHits","TrackerHitsTIBLowTof"), 
+#                          cms.InputTag("g4SimHits","TrackerHitsTIDHighTof"), 
+#                          cms.InputTag("g4SimHits","TrackerHitsTIDLowTof"), 
+#                          cms.InputTag("g4SimHits","TrackerHitsTOBHighTof"), 
+#                          cms.InputTag("g4SimHits","TrackerHitsTOBLowTof")
+    ),
+    type = cms.string('PSimHit'),
+    subdets = cms.vstring(
+        'TrackerHitsPixelBarrelHighTof',
+        'TrackerHitsPixelBarrelLowTof',
+        'TrackerHitsPixelEndcapHighTof',
+        'TrackerHitsPixelEndcapLowTof',
+#        'TrackerHitsTECHighTof',
+#        'TrackerHitsTECLowTof',
+#        'TrackerHitsTIBHighTof',
+#        'TrackerHitsTIBLowTof',
+#        'TrackerHitsTIDHighTof',
+#        'TrackerHitsTIDLowTof',
+#        'TrackerHitsTOBHighTof',
+#        'TrackerHitsTOBLowTof'
+    ),
+    crossingFrames = cms.untracked.vstring(),
+#        'MuonCSCHits',
+#        'MuonDTHits',
+#        'MuonRPCHits'),
+#)   
+        ),
+        mixHepMC = cms.PSet(
+            mixHepMCProducts
+        )
+    )
+)
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+#     simMuonCSCDigis = cms.PSet(
+#        initialSeed = cms.untracked.uint32(1234567),
+#        engineName = cms.untracked.string('TRandom3')
+#    ),
+     simSiPixelDigis = cms.PSet(
+        initialSeed = cms.untracked.uint32(1234567),
+        engineName = cms.untracked.string('TRandom3')
+   )
+)
+
+
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+  #'file:/afs/cern.ch/work/d/dkotlins/public/MC/mu_phase1/pt100_76/simhits/simHits1.root',
+  'file:/afs/cern.ch/work/d/dkotlins/public/MC/mu/pt100_74/simhits/simHits1.root',
+# gen-sim
+# '/store/relval/CMSSW_7_0_0_pre8/RelValSingleMuPt100/GEN-SIM/START70_V2_RR-v7/00000/B464EA42-2B59-E311-A2C1-0025905964C2.root',
+# '/store/relval/CMSSW_7_0_0_pre10/RelValSingleMuPt100/GEN-SIM-RECO/START70_V3-v1/00000/908DD48F-1466-E311-BEBE-0025905A48F0.root',
+# '/store/relval/CMSSW_7_0_0_pre10/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG/START70_V3-v1/00000/EAA29CCF-8965-E311-A43D-0025905A6084.root',
+  )
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    debugModules = cms.untracked.vstring('pixRecHitsValid'),
+    destinations = cms.untracked.vstring('cout'),
+#    destinations = cms.untracked.vstring("log","cout"),
+    cout = cms.untracked.PSet(
+        threshold = cms.untracked.string('ERROR')
+#        threshold = cms.untracked.string('DEBUG')
+    )
+#    log = cms.untracked.PSet(
+#        threshold = cms.untracked.string('DEBUG')
+#    )
+)
+
+
+
+process.o1 = cms.OutputModule("PoolOutputModule",
+      outputCommands = cms.untracked.vstring('drop *','keep *_*_*_TestValid'),
+#      outputCommands = cms.untracked.vstring('keep *_*_*_*'),
+      fileName = cms.untracked.string('file:dummy.root')
+)
+
+
+# DB stuff 
+# for digitisation
+
+#LA
+useLocalLASim = False
+if useLocalLASim :
+  process.LASimReader = cms.ESSource("PoolDBESSource",
+    DBParameters = 
+      cms.PSet(
+        messageLevel = cms.untracked.int32(0),
+        authenticationPath = cms.untracked.string('')
+      ),
+    toGet = cms.VPSet(
+      cms.PSet(
+        record = cms.string("SiPixelLorentzAngleSimRcd"),
+        tag = cms.string("SiPixelLorentzAngleSim_phase1_mc_v1")
+      ),
+    ),
+    #connect = cms.string('sqlite_file:../../../../../DB/phase1/SiPixelLorentzAngleSim_phase1_mc_v1.db')
+    connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+  ) # end process
+  process.lasimprefer = cms.ESPrefer("PoolDBESSource","LASimReader")
+#  end if
+
+# Quality
+useLocalQuality = False
+if useLocalQuality :
+  process.QualityReader = cms.ESSource("PoolDBESSource",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(0),
+        authenticationPath = cms.untracked.string('')
+    ),
+    toGet = cms.VPSet(cms.PSet(
+        record = cms.string('SiPixelQualityFromDbRcd'),
+        tag = cms.string('SiPixelQuality_phase1_ideal')
+    )),
+    #connect = cms.string('sqlite_file:../../../../../DB/phase1/SiPixelQuality_phase1_ideal.db')
+    connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+  )
+  process.es_prefer_QualityReader = cms.ESPrefer("PoolDBESSource","QualityReader")
+#  end if
+
+# for reco 
+# LA 
+useLocalLA = False
+if useLocalLA :
+    process.LAReader = cms.ESSource("PoolDBESSource",
+     DBParameters = cms.PSet(
+         messageLevel = cms.untracked.int32(0),
+         authenticationPath = cms.untracked.string('')
+     ),
+     toGet = cms.VPSet(
+ 	 cms.PSet(
+# LA
+	record = cms.string("SiPixelLorentzAngleRcd"),
+        tag = cms.string("SiPixelLorentzAngle_phase1_mc_v1")
+# cabling
+#           record = cms.string('SiPixelFedCablingMapRcd'),
+#           tag = cms.string('SiPixelFedCablingMap_v17')
+# GenError
+#          record = cms.string('SiPixelGenErrorDBObjectRcd'),
+#          tag = cms.string('SiPixelGenErrorDBObject38Tv1')
+#          tag = cms.string('SiPixelGenErrorDBObject38TV10')
+#          tag = cms.string('SiPixelGenErrorDBObject38T_v0_mc1')
+#          tag = cms.string('SiPixelGenErrorDBObject_38T_v1_mc')
+ 		),
+ 	),
+     #connect= cms.string('sqlite_file:../../../../../DB/phase1/SiPixelLorentzAngle_phase1_mc_v1.db')
+     connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+    ) # end process
+
+    process.LAprefer = cms.ESPrefer("PoolDBESSource","LAReader")
+
+    # now the forWidth LA
+    process.LAWidthReader = cms.ESSource("PoolDBESSource",
+     DBParameters = cms.PSet(
+         messageLevel = cms.untracked.int32(0),
+         authenticationPath = cms.untracked.string('')
+     ),
+     toGet = cms.VPSet(
+       cms.PSet(
+	record = cms.string("SiPixelLorentzAngleRcd"),
+        label = cms.untracked.string("forWidth"),
+        tag = cms.string("SiPixelLorentzAngle_phase1_mc_v1")
+       ),
+     ),
+     #connect= cms.string('sqlite_file:../../../../../DB/phase1/SiPixelLorentzAngle_phase1_mc_v1.db')
+    connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+
+    ) # end process
+
+    process.LAWidthprefer = cms.ESPrefer("PoolDBESSource","LAWidthReader")
+
+# endif
+
+# Gain 
+useLocalGain = False
+if useLocalGain :
+  process.GainsReader = cms.ESSource("PoolDBESSource",
+    DBParameters = cms.PSet(
+       messageLevel = cms.untracked.int32(0),
+       authenticationPath = cms.untracked.string('')
+    ),
+    toGet = cms.VPSet(
+      cms.PSet(
+        record = cms.string('SiPixelGainCalibrationOfflineRcd'),
+        #tag = cms.string('SiPixelGainCalibration_phase1_ideal')
+        tag = cms.string('SiPixelGainCalibration_phase1_mc_v1')
+    )),
+    #connect = cms.string('sqlite_file:../../../../../DB/phase1/SiPixelGainCalibration_phase1_mc_v1.db')
+    connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS')
+  ) # end process
+  process.Gainprefer = cms.ESPrefer("PoolDBESSource","GainsReader")
+# end if
+
+
+
+process.g4SimHits.Generator.HepMCProductLabel = 'source'
+
+# for direct digis
+process.siPixelClustersPreSplitting.src = 'simSiPixelDigis' # for V5, direct
+# process.siPixelClusters.src = 'mix'
+# modify digitizer parameters
+#process.simSiPixelDigis.digitizers.pixel.ThresholdInElectrons_BPix = 3500.0 
+#process.simSiPixelDigis.digitizers.pixel.ThresholdInElectrons_BPix = 3500.0 
+#process.simSiPixelDigis.digitizers.pixel.ThresholdInElectrons_BPix_L1 = 3500.0 
+#process.simSiPixelDigis.digitizers.pixel.ThresholdInElectrons_FPix = 3500.0 
+
+process.simSiPixelDigis.digitizers.pixel.AddPixelInefficiencyFromPython = cms.bool(False)
+process.simSiPixelDigis.digitizers.pixel.AddPixelInefficiency = cms.bool(False)
+#
+# tell the digitize it is phase1
+
+# use inefficiency from DB Gain calibration payload? OFF BY DEFAULT
+#process.simSiPixelDigis.digitizers.pixel.useDB = cms.bool(False) 
+# use LA from file 
+#process.simSiPixelDigis.digitizers.pixel.LorentzAngle_DB = cms.bool(False)
+#process.simSiPixelDigis.digitizers.pixel.TanLorentzAnglePerTesla_BPix = 0.106 
+#process.simSiPixelDigis.digitizers.pixel.TanLorentzAnglePerTesla=FPix = 0.106 
+
+# clus , label of digis 
+process.siPixelClustersPreSplitting.src = 'simSiPixelDigis'
+#process.siPixelClustersPreSplitting.ClusterThreshold = 2000.
+#process.siPixelClustersPreSplitting.ChannelThreshold = 500
+#process.siPixelClustersPreSplitting.SeedThreshold = 1000
+# set to false to ignore the gain calibration
+#process.siPixelClustersPreSplitting.MissCalibrate = cms.untracked.bool(False)
+
+
+#process.analysis = cms.EDAnalyzer("PixClusterTest",
+#    Verbosity = cms.untracked.bool(False),
+#    src = cms.InputTag("siPixelClustersPreSplitting"),
+#)
+process.analysis = cms.EDAnalyzer("PixClusterAna",
+    Verbosity = cms.untracked.bool(True),
+    #src = cms.InputTag("siPixelClusters"),
+    src = cms.InputTag("siPixelClustersPreSplitting"),
+    Select1 = cms.untracked.int32(1),  # cut on the num of dets <4 skip, 0 means 4 default 
+    Select2 = cms.untracked.int32(0),  # 6 no bptx, 0 no selection                               
+)
+
+
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('clus_histos.root')
+)
+
+#This process is to run the digitizer + clusterizer + analysis 
+#process.p1 = cms.Path(process.simSiPixelDigis)
+#process.p1 = cms.Path(process.simSiPixelDigis*process.pixeltrackerlocalreco)
+process.p1 = cms.Path(process.simSiPixelDigis*process.pixeltrackerlocalreco*process.analysis)
+
+#process.outpath = cms.EndPath(process.o1)
+
+
---
+++ HitAnalyzer/scripts/job.csh
@@ -0,0 +1,48 @@
+#!/bin/tcsh
+echo $home
+
+source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.csh
+
+#rfdir /castor/cern.ch/cms/store/data
+
+setenv SCRAM_ARCH slc6_amd64_gcc493
+cd /afs/cern.ch/user/d/dkotlins/public/CMSSW/CMSSW_8_0_0_patch2/src
+
+#source /afs/cern.ch/cms/sw/cmsset_default.csh
+source /afs/cern.ch/project/gd/apps/cms/cmsset_default.csh   # this works with gcc462
+
+# must be run frm the release area
+eval `scram runtime -csh`
+
+cd DPGAnalysis-SiPixelTools/HitAnalyzer/scripts
+pwd
+
+#  RAW
+#cmsRun runHotPixels.py 
+#cmsRun runRawDumper.py 
+#cmsRun runRawDumper_lumi.py 
+# cmsRun runRawToDigi_cfg.py 
+# cmsRun runFedErrorDumper.py
+
+#cmsRun testPxdigi.py
+
+#cmsRun PixClusterAna.py
+#cmsRun PixClusterTest.py
+#cmsRun PixClusterAna_260627.py
+
+#cmsRun PixClusterAna_Phys_254227.py
+#cmsRun PixClusterAna_Comm_248025.py
+
+#cmsRun PixClusterAna_Lumi.py
+
+#cmsRun testTracks.py
+
+cmsRun SimsToClus.py
+
+# ls /tmp/dkotlins
+
+ 
+
+
+
+
