f44a7eefe19893501df090d7d38e13cdf8187b11
Thea Aarrestad <thea.aarrestad@cern.ch>
2017-12-13 15:41:17 +0100

    replaced missing variables numberOfHits to numberOfAllHits

--- PixelTriplets/src/Pxl.cc
+++ PixelTriplets/src/Pxl.cc
@@ -2738,7 +2738,7 @@ void Pxl::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup // , e
       cout << ", dxyv " << iTrack->dxy(vtxP)*1E4 << " um";
       cout << ", dzv " << iTrack->dz(vtxP)*1E1 << " mm";
       cout << setprecision(4);
-      cout << ", hits " << hp.numberOfHits(HitPattern::TRACK_HITS);/// assuming HitCategory =TRACK_HITS = 0
+      cout << ", hits " << hp.numberOfAllHits(HitPattern::TRACK_HITS);/// assuming HitCategory =TRACK_HITS = 0
       cout << ", valid " << hp.numberOfValidTrackerHits();
       cout << endl;
     }
