2cd660d8d86d89f7a86106eee03ff0c92e4323a1
danek <danek.kotlinski@psi.ch>
2019-04-26 17:01:45 +0200

    extend the simtrack diagnostics

--- HitAnalyzer/test/SimHitAnalyzer.cc
+++ HitAnalyzer/test/SimHitAnalyzer.cc
@@ -45,7 +45,7 @@
 #include "SimDataFormats/Vertex/interface/SimVertexContainer.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-//#define HEPMC // look at generator output
+#define HEPMC // look at generator output
 #ifdef HEPMC
 //#include "SimDataFormats/GeneratorProducts/interface/HepMCProduct.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
@@ -53,6 +53,17 @@
 #include "SimGeneral/HepPDTRecord/interface/ParticleDataTable.h"
 #include "HepPDT/ParticleDataTable.hh"
 #endif
+
+// To use root histos
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+// For ROOT
+#include <TROOT.h>
+//#include <TChain.h>
+#include <TFile.h>
+#include <TF1.h>
+#include <TH2.h>
 
 using namespace std;
 using namespace edm;
@@ -63,13 +74,14 @@ using namespace edm;
 //
 
 class SimHitAnalyzer : public edm::EDAnalyzer {
-   public:
-      explicit SimHitAnalyzer( const edm::ParameterSet& );
-      ~SimHitAnalyzer();
-
-
-      virtual void analyze( const edm::Event&, const edm::EventSetup& );
-   private:
+public:
+  explicit SimHitAnalyzer( const edm::ParameterSet& );
+  ~SimHitAnalyzer();
+  void beginRun();
+  void beginJob();
+  void endJob();
+  virtual void analyze( const edm::Event&, const edm::EventSetup& );
+private:
       // ----------member data ---------------------------
   std::string HepMCLabel;
   std::string SimTkLabel;
@@ -80,7 +92,12 @@ class SimHitAnalyzer : public edm::EDAnalyzer {
   edm::EDGetTokenT<SimTrackContainer> tSimTracks;
   edm::EDGetTokenT<SimVertexContainer> tSimVertexs;
   edm::EDGetTokenT<HepMCProduct> tHepMC;
-
+  
+  // histograms 
+  TH1D *heta,*heta1,*heta2,*heta3,*heta4,*heta5,*heta6,*heta7; 
+  TH1D *heta11,*heta12,*heta13,*heta14; 
+  TH1D *hpid,*hpid1,*hpid2,*hpid3,*hpid4,*hpid5,*hpid6,*hpid7,*hpid8,*hpid9; 
+  TH1D *hpid10,*hpid11,*hpid12,*hpid13,*hpid14;
 };
 
 //
@@ -115,12 +132,10 @@ SimHitAnalyzer::SimHitAnalyzer( const edm::ParameterSet& iConfig ):
   edm::InputTag tag0("generatorSmeared","");   // for the ByToken
   tHepMC = consumes <HepMCProduct> (tag0);
 
-
 }
 
 
-SimHitAnalyzer::~SimHitAnalyzer()
-{
+SimHitAnalyzer::~SimHitAnalyzer() {
  
    // do anything here that needs to be done at desctruction time
    // (e.g. close files, deallocate resources etc.)
@@ -131,6 +146,42 @@ SimHitAnalyzer::~SimHitAnalyzer()
 //
 // member functions
 //
+void SimHitAnalyzer::beginRun() {}
+void SimHitAnalyzer::beginJob() {
+  edm::Service<TFileService> fs;
+
+  heta = fs->make<TH1D>( "heta", "eta", 30,0.,3.);
+  heta1 = fs->make<TH1D>( "heta1", "eta", 30,0.,3.);
+  heta2 = fs->make<TH1D>( "heta2", "eta", 30,0.,3.);
+  heta3 = fs->make<TH1D>( "heta3", "eta", 30,0.,3.);
+  heta4 = fs->make<TH1D>( "heta4", "eta", 30,0.,3.);
+  heta5 = fs->make<TH1D>( "heta5", "eta", 30,0.,3.);
+  heta6 = fs->make<TH1D>( "heta6", "eta", 30,0.,3.);
+  heta11 = fs->make<TH1D>( "heta11", "eta", 30,0.,3.);
+  heta12 = fs->make<TH1D>( "heta12", "eta", 30,0.,3.);
+  heta13 = fs->make<TH1D>( "heta13", "eta", 30,0.,3.);
+  heta14 = fs->make<TH1D>( "heta14", "eta", 30,0.,3.);
+
+  hpid   = fs->make<TH1D>( "hpid",  "pid",2500,0.,2500.);
+  hpid1  = fs->make<TH1D>( "hpid1", "pid",2500,0.,2500.);
+  hpid2  = fs->make<TH1D>( "hpid2", "pid",2500,0.,2500.);
+  hpid3  = fs->make<TH1D>( "hpid3", "pid",2500,0.,2500.);
+  hpid4  = fs->make<TH1D>( "hpid4", "pid",2500,0.,2500.);
+  hpid5  = fs->make<TH1D>( "hpid5", "pid",2500,0.,2500.);
+  hpid6  = fs->make<TH1D>( "hpid6", "pid",2500,0.,2500.);
+  hpid7  = fs->make<TH1D>( "hpid7", "pid",2500,0.,2500.);
+  hpid8  = fs->make<TH1D>( "hpid8", "pid",2500,0.,2500.);
+  hpid9  = fs->make<TH1D>( "hpid9", "pid",2500,0.,2500.);
+  hpid10 = fs->make<TH1D>( "hpid10", "pid",2500,0.,2500.);
+  hpid11 = fs->make<TH1D>( "hpid11", "pid",2500,0.,2500.);
+  hpid12 = fs->make<TH1D>( "hpid12", "pid",2500,0.,2500.);
+
+}
+void SimHitAnalyzer::endJob() {}
+
+
+
+
 
 // ------------ method called to produce the data  ------------
 void
@@ -138,6 +189,7 @@ SimHitAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup
 {
    using namespace edm;
    const float PI = 3.1416;
+   const bool print = false;
 
    std::vector<PSimHit> theTrackerHits;
    std::vector<SimTrack> theSimTracks;
@@ -201,19 +253,18 @@ SimHitAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup
 
 
 #ifdef HEPMC
-   //Handle< HepMCProduct > EvtHandle ;
-   //iEvent.getByLabel( "generator", EvtHandle ) ;
-   const HepMC::GenEvent* myGenEvent = MCEvt->GetEvent() ;
-   
-   //Hepmc::GenEvent * myGenEvent = new  HepMC::GenEvent(*(MCEvt->GetEvent()));
-   
-   cout<<" Print HepMC "<<endl;
-   int i=0;
-   for ( HepMC::GenEvent::particle_const_iterator p = myGenEvent->particles_begin();
-	 p != myGenEvent->particles_end(); ++p, ++i ) {
-     cout<< "Particle from MC = "<<i<<" type "<< abs((*p)->pdg_id())<<" "<< (*p)->status()
-	 << " ,Pt/Eta = "<< (*p)->momentum().perp()<<" "<< (*p)->momentum().eta()<<endl;  
-   }   
+   const bool printGenParticles = false;
+   if(printGenParticles) {
+     const HepMC::GenEvent* myGenEvent = MCEvt->GetEvent() ;
+     if(print) cout<<" Print HepMC "<<endl;
+     int i=0;
+     for ( HepMC::GenEvent::particle_const_iterator p = myGenEvent->particles_begin();
+	   p != myGenEvent->particles_end(); ++p, ++i ) {
+       if(print) 
+	 cout<< "Particle from MC = "<<(i+1)<<" type "<< abs((*p)->pdg_id())<<" status "<< (*p)->status()
+	     << " ,Pt/Eta = "<< (*p)->momentum().perp()<<" "<< (*p)->momentum().eta()<<endl;  
+     }   
+   }
 #endif // HEPMC
 
    const bool printTracks = true;
@@ -221,27 +272,74 @@ SimHitAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup
      int i=0;
      for (std::vector<SimTrack>::iterator isimtk = theSimTracks.begin();
 	  isimtk != theSimTracks.end(); ++isimtk, ++i){
-       cout<<"Track "<<i<<" genIndex "<<isimtk->genpartIndex() <<" Vtx index "<<isimtk->vertIndex()
-	   <<" momentum Pt "<< sqrt(isimtk->momentum().perp2())<<" eta "<< isimtk->momentum().eta()<<endl;
+
+       auto pid = abs(isimtk->type());
+       auto p = sqrt(isimtk->momentum().Vect().Mag2());
+       auto pt =  sqrt(isimtk->momentum().Vect().Perp2());
+       auto eta = abs(isimtk->momentum().eta());
+       auto ch = isimtk->charge();
+       auto gen = isimtk->genpartIndex();
+      if(print) 
+	cout<<"Track "<<(i+1)<<" genIndex "<<gen<<" Vtx index "<<isimtk->vertIndex()
+	   <<" type "<<pid<<" ch "<<ch 
+	   <<" p " << p<<" pt "<< pt<<" eta "<< eta
+	   <<endl;
+
+      hpid->Fill(float(pid));
+      if(abs(eta)>3.0) continue; // skip
+      hpid1->Fill(float(pid));
+
+      if(ch==0) { // neutrals
+	hpid2->Fill(float(pid));
+	if(p<0.0001) continue; // skip netrals below 100keV
+	hpid3->Fill(float(pid));
+      } else { // charged
+	hpid4->Fill(float(pid));
+	if(pt<0.100) continue; // skip chartge below 100MeV
+	hpid5->Fill(float(pid));
+      }
+
+      hpid6->Fill(float(pid));
+      heta->Fill(eta);
+
+      if( (pid==13) ||  (pid==211) ||  (pid==321) ||  (pid==2212) ) heta1->Fill(eta); // charged 
+      else if( (pid==11) ) heta2->Fill(eta);   // e
+      else if( (pid==22) ) heta3->Fill(eta);   // gamma
+      else if( (pid==2112) ) heta4->Fill(eta); // n
+      else if( (pid==111) ) heta5->Fill(eta);  //pi0
+      else  heta6->Fill(eta);  // left 
+
+      if(gen>-1) { // primary 
+	hpid7->Fill(float(pid));
+	if(ch==0) {heta11->Fill(eta);hpid8->Fill(float(pid));}
+	else      {heta12->Fill(eta);hpid9->Fill(float(pid));}
+      } else {  // secondary 
+	hpid10->Fill(float(pid));
+	if(ch==0) {heta13->Fill(eta);hpid11->Fill(float(pid));}
+	else      {heta14->Fill(eta);hpid12->Fill(float(pid));}
+      }
+
      }
    }
-   const bool printVertices = true;
+
+
+   const bool printVertices = false;
    if(printVertices) {
      int i=0;
      for (std::vector<SimVertex>::iterator isimvtx = theSimVertexes.begin();
 	  isimvtx != theSimVertexes.end(); ++isimvtx, ++i){
-       cout<<"Vertex "<<i
+       if(print) cout<<"Vertex "<<i
 	   <<" position  x = "<<isimvtx->position().x() <<" y = "<<isimvtx->position().y() 
 	   <<" z = "<< isimvtx->position().z()
-	   <<" index = "<< isimvtx->vertexId()<<" parent = "<< isimvtx->parentIndex()
+	   <<" vertex index = "<< isimvtx->vertexId()<<" parent = "<< isimvtx->parentIndex()
 	   <<" process = "<<isimvtx->processType() 
 	   <<endl;
      }
    }
 
-   const bool printsHits = true;
+   const bool printsHits = false;
    if(printsHits) {
-     cout<<" SimHits "<<theTrackerHits.size()<<endl;
+     if(print) cout<<" SimHits "<<theTrackerHits.size()<<endl;
      std::map<unsigned int, std::vector<PSimHit>,std::less<unsigned int> > SimHitMap;
      for (std::vector<PSimHit>::iterator isim = theTrackerHits.begin();
 	  isim != theTrackerHits.end(); ++isim){
@@ -254,6 +352,7 @@ SimHitAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup
 	 float eloss = (*isim).energyLoss() * 1000000/3.7;//convert GeV to ke 
 	 //float tof = (*isim).timeOfFlight();
 	 float p = (*isim).pabs();
+	 //float pt = (*isim).momentumAtEntry().perp();
 	 float theta = (*isim).thetaAtEntry();
 	 float beta = 0;  // beta is roughly like real theta
 	 if(theta<PI/2.) beta=(PI/2.) - theta; //  
@@ -264,10 +363,10 @@ SimHitAnalyzer::analyze( const edm::Event& iEvent, const edm::EventSetup& iSetup
 	 int pid = ((*isim).particleType()); 
 	 int tid = (*isim).trackId();
 	 int procType = (*isim).processType();
-	 cout<<pid<<endl;
+	 //cout<<pid<<endl;
 	 //if(pid==13) 
-	 cout<<"Pix mu Simhit id "<<pid<<" track "<<tid<<" proc "<<procType<<" de "
-	       <<eloss<<" p "<<p<<" theta "<<theta<<" beta "<<beta<<" eta "<<eta
+	 if(print) cout<<"Simhit id "<<pid<<" track "<<tid<<" proc "<<procType<<" de "
+	       <<eloss<<" p "<<p<<" theta "<<theta
 	       <<" position  x = "<<isim->localPosition().x() 
 	       <<" y = "<<isim->localPosition().y() <<" z = "<< isim->localPosition().z()<<endl;
        }
--- HitAnalyzer/test/SimHitAnalyzer.py
+++ HitAnalyzer/test/SimHitAnalyzer.py
@@ -5,14 +5,30 @@ process = cms.Process("Analysis",eras.Run2_2017)
 # run the input file through the end;
 # for a limited number of events, replace -1 with the desired number 
 #
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(2) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 #process.load( "SimGeneral.HepPDTESSource.pythiapdt_cfi" )
 
 process.source = cms.Source( "PoolSource",
                              fileNames = cms.untracked.vstring(
-#			     'file:../scripts/sim2.root'
-    'file:/afs/cern.ch/work/d/dkotlins/public//MC/mu_phase1/pt100/simhits/simHits1.root'
+
+#  '/store/user/kotlinski/MC/mu_pt100/simhits/simHits1.root'
+
+# mb
+#"/store/relval/CMSSW_10_4_0/RelValMinBias_13/GEN-SIM/103X_mc2017_realistic_v2-v1/20000/F0FF4DFA-613B-4749-BBC3-FB29AD691FA5.root"
+
+"/store/relval/CMSSW_10_4_0/RelValMinBias_13/GEN-SIM/103X_upgrade2018_design_v4-v1/20000/C9CE0381-F732-4A4F-A263-D445DF99148C.root" # use this
+
+#"/store/relval/CMSSW_10_4_0/RelValMinBias_13/GEN-SIM/103X_upgrade2018_realistic_v8-v1/20000/E0E79FD8-5A76-6145-9816-B7BBC5E79AB1.root" 
+
+# PU 
+#"/store/relval/CMSSW_10_4_0/RelValTTbar_13/GEN-SIM-DIGI-RAW/PU25ns_103X_upgrade2018_design_v4-v1/20000/193FB1CE-333D-E540-995E-1BA38BA1CE3C.root"
+
+#"/store/relval/CMSSW_10_4_0/RelValTTbar_13/GEN-SIM-DIGI-RAW/PU25ns_103X_upgrade2018_realistic_v8-v1/20000/0D388168-67C8-A544-8A75-8A96D40C396B.root"
+
+#"/store/relval/CMSSW_10_4_0/RelValTTbar_13/GEN-SIM-DIGI-RAW/PU25ns_103X_mc2017_realistic_v2-v1/20000/222AF93E-CE66-6342-A266-E78F6FCDD8EC.root"
+
+
 #			     'file:$home/work/MC/mu/pt100_pre10/simhits/simHits1.root'
 #			     'file:$home/work/MC/mu/pt100_71_pre5/simhits/simHits1.root'
 #			     'file:$home/work/MC/mu/pt100_71_pre5/simhits/simHits1_eta0p1.root'
