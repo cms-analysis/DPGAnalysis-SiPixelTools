085941061162bca4863ea75c10125a07f8206ccc
Danyyl Brzhechko <debrz@Danyyls-MacBook-Pro.local>
2020-04-28 15:56:48 +0200

    reindent problem

---
+++ PixelHitAssociator/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DPGAnalysis-SiPixelTools/PixelHitAssociator/',1)[0])+'/cfipython/slc7_amd64_gcc820/DPGAnalysis-SiPixelTools/PixelHitAssociator')
