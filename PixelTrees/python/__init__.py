eac8c1bf486137ed4ff86554525bd4b20b93a126
Korbinian Schweiger <korbinian.schweiger@cern.ch>
2020-04-29 09:10:53 +0200

    Removed unnecessary files

--- PixelTrees/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DPGAnalysis-SiPixelTools/PixelTrees/',1)[0])+'/cfipython/slc7_amd64_gcc820/DPGAnalysis-SiPixelTools/PixelTrees')
